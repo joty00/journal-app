c3060798a1b556b03e45baa15e02f38e246f5428
joty00 <jorgitocastilla27@gmail.com>
2023-03-23 00:34:44 -0400

    error de la data vacia arreglado

--- src/modules/daybook/store/journal/actions.js
+++ src/modules/daybook/store/journal/actions.js
@@ -3,6 +3,10 @@ import journalApi from '@/api/journalApi.js'
 export const loadEntries = async ({ commit }) => {
 
     const { data } = await journalApi.get('/entries.json')
+    if (!data) {
+        commit('setEntries', [])
+        return
+    }
     const entries = []
     for (let id of Object.keys(data)) {
         entries.push({
